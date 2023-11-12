0523c5751499533b25b3f914f08159f265aeb4b8
Vitaliy <vitaliyufc@gmail.com>
2023-11-12 09:00:15 +0300

    Initial commit

---
+++ Truck.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Truck extends Machine
+{
+   void doWork() { System.out.println("Truck is started"); }
+
+   void stopWork() { System.out.println("Truck is stoped"); }
+}
